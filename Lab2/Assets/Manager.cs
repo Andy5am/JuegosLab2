d1a733bfe702da9364ad341132aea482223535ef
Andy5am <38666859+Andy5am@users.noreply.github.com>
2019-03-31 11:38:05 -0600

    juego

---
+++ Lab2/Assets/Manager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject currentPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+            if (player && !currentPlayer)
+                currentPlayer = Instantiate(player, new Vector3(-15, 3, 3), Quaternion.identity);
+    }
+}
